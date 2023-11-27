73388cce064e1f203424611f03e72c8e90b1c1e0
VictorDiasO <victordiasdeveloper@gmail.com>
2023-11-27 20:26:40 -0300

    Starting the landing page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import TitleSection from '@/components/landing-page/title-section'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
+import AppBanner from '../../../public/assets/images/appBanner.png';
 
 const HomePage = () => {
   return (
@@ -11,6 +14,28 @@ const HomePage = () => {
           pill="✨ Your Workspace, Perfected"
           title="All-In-One Collaboration and Productivity Platform"
         />
+        <div
+          className='bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]'
+        >
+          <Button
+            variant='btn-secondary'
+            className='w-full rounded-[10px] p-6 text-2xl bg-background'
+          >
+            Get Flowcus Free
+          </Button>
+        </div>
+        <div
+          className='md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]'
+        >
+          <Image
+            src={AppBanner}
+            alt='Application Banner'
+          />
+          <div
+            className='bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'
+          >
+          </div>
+        </div>
       </div>
     </section>
   )
