1715213255f82b16fe6ad27b0887c7f24299a983
VictorDiasO <victordiasdeveloper@gmail.com>
2023-11-27 23:31:16 -0300

    Adding images | Creating the constants file | Keep working on the Landing Page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,11 +3,12 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 import AppBanner from '../../../public/assets/images/appBanner.png';
+import { CLIENTS } from '@/lib/constants';
 
 const HomePage = () => {
   return (
-    <section>
-      <div
+    <>
+      <section
         className='overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center'
       >
         <TitleSection
@@ -36,8 +37,58 @@ const HomePage = () => {
           >
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className='relative'>
+        <div className="overflow-hidden
+          flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute"
+        >
+          {[...Array(2)].map((arr) => (
+            <div
+              key={arr}
+              className='flex flex-nowrap animate-slide'
+            >
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className='relative w-[200px] m-20 shrink-0 flex items-center'
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className='object-contain max-w-none'
+                  />
+                </div>
+              ))}
+            </div>
+            )
+          )}
+        </div>
+      </section>
+    </>
   )
 }
 
